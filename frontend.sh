e1870c9c0cfdc071c7d517517269949b42019b13
devops19 <bhashithagedela@gmail.com>
2024-03-05 15:08:03 +0530

    adding shell scripting topics

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,19 @@
-echo -e "\e[36m installing nginx \e[0m"
+echo -e "\e[36m installing Nginx \e[0m"
 dnf install nginx -y
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo -e "\e[36m Copy Expense Config file \e[0m"
+cp expense.conf /etc/Nginx/default.d/expense.conf
 
+echo -e "\e[36m Clean old Nginx Content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[36m Download Frontend Application Code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[36m Extract Downloaded Application Content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[36m Starting Nginx Service \e[0m"
 systemctl enable nginx
 systemctl restart nginx
