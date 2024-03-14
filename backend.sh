88ea3c95b2019d661159fcdc0b0a2279bda78db0
devops19 <bhashithagedela@gmail.com>
2024-03-14 15:17:41 +0530

    shell scripting topics

--- backend.sh
+++ backend.sh
@@ -8,6 +8,7 @@ if [ $? -eq 0]; then
  echo -e "\e[32m SUCCESS [\e0m"
 else
  echo -e "\e[31m FAILURE [\e0m"
+ fi
 
 echo -e "${clor} Enable Nodejs 18 Version \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
