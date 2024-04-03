f4db8c3d175c2d02ee927b2650e561e42b94b6dc
devops19 <bhashithagedela@gmail.com>
2024-04-03 11:38:56 +0530

    expense-ansible

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ dnf install nginx -y &>>$log_file
 echo $?
 
 echo -e "${color} Copy Expense Config file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+cp expense.conf /etc/nginx/default.d/ expense.conf &>>$log_file
 echo $?
 
 echo -e "${color} Clean old Nginx Content \e[0m"
@@ -18,7 +18,7 @@ echo $?
 
 echo -e "${color} Extract Downloaded Application Content \e[0m"
 cd /usr/share/nginx/html &>>$log_file
-unzip /tmp/frontend.zip &>>$log_file
+   unzip //tmp/frontend.zip &>>$log_file
 echo $?
 
 echo -e "${color} Starting Nginx Service \e[0m"
